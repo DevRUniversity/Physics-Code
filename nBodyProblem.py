3f72483428a4aca5b15e82d81559654ee829830b
DevRUniversity <DevonRUniversity@protonmail.com>
2022-04-23 22:17:41 -0400

    NBP - v3

--- nBodyProblem.py
+++ nBodyProblem.py
@@ -1,10 +1,10 @@
 from matplotlib import pyplot as plt
 import math
 import time
-seconds_of_computation_time = 30
+seconds_of_computation_time = 10
 Gravitational_constant = 0.00000000006674
-bounds_of_calculation_left = -10000
-bounds_of_calculation_right = 10000
+bounds_of_calculation_left = -50000
+bounds_of_calculation_right = 50000
 ListOfGravitationalObjects = []
 newX = []
 newY = []
@@ -17,23 +17,21 @@ class Gravitational_Object:
     x_velocity = 0
     y_velocity = 0
     mass = 1 #kg
+    color = "black"
 
-    def __init__(self, x, y,v_x,v_y,inMass):
+    def __init__(self, x, y,v_x,v_y,inMass, inColor):
         self.x_cord = x
         self.y_cord = y
         self.x_velocity = v_x
         self.y_velocity = v_y
         self.mass = inMass
-
-    def get_force_from_points(self, distance_x, distance_y,distance_z, mass_two):
-        magnitude =( (abs(distance_x - x_cord) ** 2) + (abs(distance_y - y_cord) ** 2))**0.5
-        return (Gravitational_constant*mass_two)/magnitude
+        self.color = inColor
 
     def accelerate_this_by(self,distance_x,distance_y,mass_two):
         magnitude = ((abs(distance_x - self.x_cord) ** 2) + (abs(distance_y - self.y_cord) ** 2)) ** 0.5
-        acceleration = (Gravitational_constant*mass_two)/magnitude
-        x_unit_component_of_velocity_times_acceleration = float(abs(distance_x - self.x_cord) )/ magnitude * acceleration
-        y_unit_component_of_velocity_times_acceleration = float(abs(distance_y - self.y_cord) )/ magnitude * acceleration
+        acceleration = (Gravitational_constant*mass_two)/(magnitude**2)
+        x_unit_component_of_velocity_times_acceleration = float(distance_x - self.x_cord )/ magnitude * acceleration
+        y_unit_component_of_velocity_times_acceleration = float(distance_y - self.y_cord )/ magnitude * acceleration
         self.x_velocity = self.x_velocity + x_unit_component_of_velocity_times_acceleration
         self.y_velocity = self.y_velocity + y_unit_component_of_velocity_times_acceleration
 
@@ -44,7 +42,7 @@ class Gravitational_Object:
 def current_milli_time():
     return round(time.time() * 1000)
 
-def animate(x_0, y_0, x_1, y_1):
+def animate(x_0, y_0, x_1, y_1, obj_color):
     newX.append(x_1)
     newY.append(y_1)
     oldX.append(x_0)
@@ -55,19 +53,23 @@ def animate(x_0, y_0, x_1, y_1):
     plt.ylim(bounds_of_calculation_left, bounds_of_calculation_right)
 
     # Ploting graph
-    plt.scatter(newX, newY, color='blue')
+    plt.scatter(newX, newY, color=obj_color)
     #plt.scatter(oldX, oldY, color='white')
     plt.pause(0.0001)
 
-GravObj1 = Gravitational_Object(0,0,10,10,10000)
-GravObj2 = Gravitational_Object(100,-200,10,10,1000)
+GravObj1 = Gravitational_Object(0,0,0,0,29890000000000000000,"blue")
+GravObj2 = Gravitational_Object(-1000,2000,100,-50,29890000000000000000,"red")
+GravObj3 = Gravitational_Object(1000,-2000,-1000,0,100,"green")
 ListOfGravitationalObjects.append(GravObj1)
 ListOfGravitationalObjects.append(GravObj2)
+ListOfGravitationalObjects.append(GravObj3)
 
 elapsed_milliseconds = 0
 initial_milliseconds = current_milli_time()
 counter = 0
 while(elapsed_milliseconds / 1000 < seconds_of_computation_time):
+    if(int(elapsed_milliseconds / 1000) % 10):
+        print(str(elapsed_milliseconds/ 10 / seconds_of_computation_time) + "% Done with Simulation" )
     PreviousGravObjs = ListOfGravitationalObjects.copy()
     for objN1 in ListOfGravitationalObjects:
         for objN2 in ListOfGravitationalObjects:
@@ -76,8 +78,8 @@ while(elapsed_milliseconds / 1000 < seconds_of_computation_time):
     for i in range(0,len(ListOfGravitationalObjects)):
         ListOfGravitationalObjects[i].update_positon() #called here to avoid strange issues with the order in which calculations are made
         animate(PreviousGravObjs[i].x_cord,PreviousGravObjs[i].y_cord,
-                ListOfGravitationalObjects[i].x_cord,ListOfGravitationalObjects[i].y_cord)
-        
+                ListOfGravitationalObjects[i].x_cord,ListOfGravitationalObjects[i].y_cord,ListOfGravitationalObjects[i].color)
+
     elapsed_milliseconds = current_milli_time() - initial_milliseconds
-    print(str(elapsed_milliseconds / 1000) + "of Computer Time Used")
+    #print(str(elapsed_milliseconds / 1000) + "of Computer Time Used")
 plt.show()
